b6ab34ba4dabf26802eef01210a8379970d2251e
David66775 <davidjkeesling@gmail.com>
2026-04-17 20:08:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-let setkey = localStorage.getItem("key");
-let keyset = localstorage.getItem("set");
-let key = "";
-let e = document.getElementById("e");
-let e1 = document.getElementById("e1");
-if (keyset !== null && keyset !== false) {
-  key = setkey;
-}
-if (keyset === null) {
-  keyset === false;
-}
-function save() {
-  localStorage.setItem("key", key);
-  localStorage.setItem("set", true);
-}
 document.addEventListener("keydown", function(e) {
-  if (e.key === key) {
+  if (e.key === "5" && event.ctrlKey) {
     window.open("https://david66775.github.io/MyEditor", "_blank");
   }
 });
